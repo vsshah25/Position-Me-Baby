50dba1ab1756f9b619f45a7647201a457e35536e
pranav1001 <pranavsankhe40@gmail.com>
2016-10-23 12:28:12 +0530

    serial communication code added

--- python_code.py
+++ python_code.py
@@ -15,8 +15,9 @@ blue = (0,0,255)
 display_surface = pygame.display.set_mode((window_width,window_height),0,32)
 display_surface.fill(white)
 pygame.display.set_caption("map_69")
+myfont = pygame.font.SysFont("monospace", 20)
 
-
+		
 class transmitter:
 	def __init__(self, trans_id):
 		self.trans_id = trans_id
@@ -25,34 +26,35 @@ class transmitter:
 		self.x = 450 - 450*int(bool(self.trans_id <3))
 		self.y = (450 - 450*int(bool(self.trans_id%2 !=0)))
 		pygame.draw.rect(display_surface,(0,255,0),(self.x ,self.y,50,50))
-		'''
-		self.a = int(475*(int(bool(self.trans_id%2 !=0))))
-		self.b = int(25*(int(bool(self.trans_id%2 =0))))
-		self.c = int(475*(int(bool(self.trans_id >=3))))
-		self.d = int(25*(int(bool(self.trans_id <3))))
+		
+		a = int(bool(self.trans_id %2 != 0))
+		b = int(bool(self.trans_id %2 == 0))
+		c = int(bool(self.trans_id >= 3))
+		d = int(bool(self.trans_id < 3))
 		label = myfont.render(str(self.trans_id), 1, (0,0,0))
-		display_surface.blit(label, (a+b, c+d, 25))
-		'''
+		display_surface.blit(label, (25*a + 475*b, 475*c + 25*d))
+
+
 
 	self.serial = serial.Serial('/dev/ttyACM0')
     
 
 	
 	def send_serial(self):
-		while(1):
-			c = self.serial.write(self.trans_id)
-			if self.serial.read() == 0 
-				break
+			for i in range(0,5):
+				self.serial.write(self.trans_id)
+			
 
-def recieve_serial(self):
-	r_data  = self.serial.read()
-	self.time = r_data                                            #will be calculted by the arduino                                                
+	def recieve_serial(self):
+		r_data  = self.serial.read()
+		self.time = r_data
+		print(self.time)
+		self.radius = (time/1000)*340 
+		pygame.draw.circle(display_surSface, blue, (25*a + 475*b, 475*c + 25*d), self.radius, 1)                                                  
 				
 
 
 
-
-myfont = pygame.font.SysFont("monospace", 20)
 
 trans_1  = transmitter(1)
 trans_2  = transmitter(2)
