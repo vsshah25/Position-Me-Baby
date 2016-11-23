66149213f26de39983fcf014302467afd3b8c011
Pranav Sankhe <pranavsankhe40@gmail.com>
2016-11-23 19:12:36 +0530

    test serial transfer

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -1,24 +1,34 @@
 #include <SoftwareSerial.h>
 SoftwareSerial mySerial(10, 11); // RX, TX
 
-long long int time = 0;
-
+long int time = 0;
+int readings[10];
+int sum = 0;
 void setup()
 {
   Serial.begin(9600);
-  pinMode(interrupt_pin, INPUT);
-  //attachInterrupt(interrupt_pin, send_data, HIGH);
 }
 
 void loop()
 {
-  time = millis();
-  //Serial.println(digitalRead(2));
-  if(digitalRead(2) == 1) 
+  
+  for(int i=0;i<10;i++)
   {
-    mySerial.write(time);
-    Serial.println(time);
+  readings[i] = digitalRead(2);
+  //Serial.print(i);
+  //Serial.print("  ");
+  //Serial.println(readings[i]);
   }
+  if(readings[0]==1 &&readings[1]==1 && readings[2]==1 &&readings[3]==1 &&readings[4]==1 &&readings[5]==1 &&readings[6]==1 &&readings[7]==1 &&readings[8]==1 &&readings[9]==1 )
+  {
+  time = millis();
+  //Serial.print("time");
+
+  Serial.print(time);
+  Serial.print("@");
+  
+  }
+ delay(10);
+  
+ 
 }
-
-
