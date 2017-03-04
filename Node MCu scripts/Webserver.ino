8f5751f55501bab76fd0c0c4d8ef8224f2b9f4da
saqib1707 <azimsaqib96@gmail.com>
2017-03-05 01:33:17 +0530

    madarchod

--- Node MCu scripts/Webserver.ino
+++ Node MCu scripts/Webserver.ino
@@ -7,12 +7,13 @@ WiFiUDP UDP;
 const char *ssid = "TP LINK Wifi";
 const char *password = "devsak49";
 
-const uint16_t UDP_LOCAL_PORT =  8050;
-const uint16_t UDP_REMOTE_PORT = 8051;
-const char UDP_REMOTE_HOST[] =   "192.168.0.124";
-//char rssi[] = String(WiFi.RSSI(), 5);
+const uint16_t UDP_LOCAL_PORT =  8051;
+char TRIGGER_STRING[] = "node_";
 
-//WiFiServer server(80);
+char rssi[4][10];   // decide the postion of this rssi matrix declaration
+                                                   
+WiFiServer server(80);
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -32,46 +33,96 @@ void setup() {
     delay(500);
     Serial.print(".");
   }
-  // Start the server
-  //server.begin();
-  //Serial.println("Server started");
 
   Serial.println("");
   Serial.println("WiFi connected"); 
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 
+  // Start the server
+  server.begin();
+  Serial.println("Server started");
 
-Serial.println("PROGRAM: starting UDP");
-if (UDP.begin(UDP_LOCAL_PORT) == 1)
-{
-  Serial.println("PROGRAM: UDP started");
+
+  Serial.println("PROGRAM: starting UDP");
+  if (UDP.begin(UDP_LOCAL_PORT) == 1)
+  {
+    Serial.println("PROGRAM: UDP started");
+  }
+  else
+  {
+    Serial.println("PROGRAM: UDP not started");
+  }
 }
-else
-{
-  Serial.println("PROGRAM: UDP not started");
-} 
-}
+
 
 void loop() {
-  String rssi = String(WiFi.RSSI());
-  //int rssi = WiFi.RSSI();
-char TRIGGER_STRING[3];
- for(int i = 0; i!=3; i++){
-  TRIGGER_STRING[i] = rssi[i];
-}
-//Serial.println(String(TRIGGER_STRING));
-  // put your main code here, to run repeatedly:
-  //char rssi[] = "Saqib";
-pinMode(2, LOW);
-delay(1000);
-Serial.println("PROGRAM: sending trigger");
-UDP.beginPacket(UDP_REMOTE_HOST, UDP_REMOTE_PORT);
-UDP.write(TRIGGER_STRING);
+//------------------------------------------
+  char packetBuffer[UDP_TX_PACKET_MAX_SIZE]; // buffer(array) to hold incoming packet,
+  int packetSize = UDP.parsePacket();
 
-UDP.endPacket();
+  // the server checks whether there is any packet in its buffer
+  if (packetSize > 0)
+  {
+    Serial.print("PROGRAM: received UDP packet of size ");
+    Serial.println(packetSize);
+    Serial.print("PROGRAM: from ");
+    IPAddress remote = UDP.remoteIP();        // extracts the remote IP of the client
+    for (int i = 0; i < 4; i++)
+    {
+      Serial.print(remote[i], DEC);
+      if (i < 3)
+      {
+        Serial.print(".");
+      }
+    }
+    Serial.print(", port ");
+    Serial.println(UDP.remotePort());
+    UDP.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);    // extract the data from the packet
+    packetBuffer[packetSize] = '\0';
+    Serial.print("PROGRAM: contents: ");
+    Serial.println(packetBuffer);
 
-pinMode(2, HIGH);
-delay(1000);
+    int char_to_cmp = 6;                 // chars comparing = node_
+    if (strncmp(packetBuffer, TRIGGER_STRING, char_to_cmp) == 0)
+    {
+      Serial.println("PROGRAM: trigger received");
+      // DO YOUR STUFF HERE
+      int node_id;
+      Serial.print("PROGRAM: Data received from node-");
+      // identify the node
+      switch(packetBuffer[5]){
+        
+        case 1:
+          // node 1
+          node_id = 0;
+          break;
+        case 2:
+          // node 2
+          node_id = 1;
+          break;
+        case 3:
+          // node 3
+          node_id = 2;
+          break;
+        case 4:
+          // node 4
+          node_id = 3;
+          break;
+        default:
+          node_id = 5;
+          break;
+      }
+      Serial.println(node_id);
 
+      // Extract the rssi value from the packetBuffer only
+      for (int j = 6, int k = 0; packetBuffer[j]!='\0'; j++, k++){
+        rssi[node_id][k] = packetBuffer[j];
+      }
+      delay(200);
+    }
+    delay(10);
+    // Add an extra delay for the other node mcu's to transmit to maintain a periodic 
+    // cycle among the four node mcu's
+  }
 }
--- Node MCu scripts/Wificlient.ino
+++ Node MCu scripts/Wificlient.ino
@@ -3,13 +3,14 @@
 
 WiFiUDP UDP;
 
-
 const char *ssid = "TP LINK Wifi";
 const char *password = "devsak49";
 
+const uint16_t UDP_LOCAL_PORT =  8050;
+const uint16_t UDP_REMOTE_PORT = 8051;        // Must match UDP_REMOTE_PORT on receiver
+const char UDP_REMOTE_HOST[] =   "192.168.0.124";
 
-const uint16_t UDP_LOCAL_PORT =  8051; // Must match UDP_REMOTE_PORT on sender
-char TRIGGER_STRING[] = "";  // Must match TRIGGER_STRING on sender
+char TRIGGER_STRING[] = "node_1";           // Must match TRIGGER_STRING on receiver
 
 void setup() {
   // put your setup code here, to run once:
@@ -35,52 +36,41 @@ void setup() {
   Serial.println(WiFi.localIP());
 
 
-Serial.println("PROGRAM: starting UDP");
-if (UDP.begin(UDP_LOCAL_PORT) == 1)
-{
-  Serial.println("PROGRAM: UDP started");
-}
-else
-{
-  Serial.println("PROGRAM: UDP not started");
-}
+  Serial.println("PROGRAM: starting UDP");
+  if (UDP.begin(UDP_LOCAL_PORT) == 1)
+  {
+    Serial.println("PROGRAM: UDP started");
+  }
+  else
+  {
+    Serial.println("PROGRAM: UDP not started");
+  }
 
 }
+
+// Suppose this is for node_i
 
 void loop() {
   // put your main code here, to run repeatedly:
-  char packetBuffer[UDP_TX_PACKET_MAX_SIZE]; // buffer to hold incoming packet,
-int packetSize = UDP.parsePacket();
+  String rssi = String(WiFi.RSSI());
+  //int rssi = WiFi.RSSI();
+  char TRIGGER_STRING[3];
+  for(int i = 0; i!=3; i++){
+    TRIGGER_STRING[i] = rssi[i];
+  }
+  //Serial.println(String(TRIGGER_STRING));
+  // put your main code here, to run repeatedly:
+  //char rssi[] = "Saqib";
 
-if (packetSize > 0)
-{
-  Serial.print("PROGRAM: received UDP packet of size ");
-  Serial.println(packetSize);
-  Serial.print("PROGRAM: from ");
-  IPAddress remote = UDP.remoteIP();
-  for (int i = 0; i < 4; i++)
-  {
-    Serial.print(remote[i], DEC);
-    if (i < 3)
-    {
-      Serial.print(".");
-    }
-  }
-  Serial.print(", port ");
-  Serial.println(UDP.remotePort());
-  UDP.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
-  packetBuffer[packetSize] = '\0';
-  Serial.print("PROGRAM: contents: ");
-  Serial.println(packetBuffer);
-  if (strncmp(packetBuffer, TRIGGER_STRING, strlen(TRIGGER_STRING)) == 0)
-  {
-    Serial.println("PROGRAM: trigger received");
-    // DO YOUR STUFF HERE
-  }
-  delay(10);
-}
-//else{
-//  Serial.println("No packets received");
-//}
+  pinMode(2, LOW);
+  delay(1000);
+  Serial.println("PROGRAM: sending rssi data");
+  UDP.beginPacket(UDP_REMOTE_HOST, UDP_REMOTE_PORT);
+  UDP.write(TRIGGER_STRING);
+
+  UDP.endPacket();
+
+  pinMode(2, HIGH);
+  delay(1000);
 
 }
